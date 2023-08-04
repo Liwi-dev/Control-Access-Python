ef219bf93b43cbdab5d4fb3d5317efcf7c7b9e07
Liwi-dev <piter.formateos@gmail.com>
2023-08-04 05:46:50 -0400

    se elimina salir_registro_dni

--- Aplicacion.py
+++ Aplicacion.py
@@ -62,30 +62,7 @@ def eliminar_registro(registros, identificador):
     registros = [registro for registro in registros if registro.dni != identificador and registro.patente != identificador]
     return registros
 
-
-    
-def salir_registro_dni():
-    dni = input("Número de RUN: ")
-
-    with open("registros.txt", "r") as archivo:
-        lineas = archivo.readlines()
-
-    encontrado = False
-    for i, linea in enumerate(lineas):
-        datos = linea.strip().split(",")
-        if datos[1] == dni and datos[4] == "":
-            hora_salida = datetime.datetime.now()
-            lineas[i] = f"{datos[0]},{datos[1]},{datos[2]},{datos[3]},{hora_salida}\n"
-            encontrado = True
-            break
-
-    if encontrado:
-        with open("registros.txt", "w") as archivo:
-            archivo.writelines(lineas)
-        print("Registro de salida registrado.")
-    else:
-        print("No se encontró un registro de ingreso para el DNI proporcionado.")
-
+  
 
 def salir_registro_patente():
     dni = input("Patente del Vehiculo: ")
